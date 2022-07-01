8b54645cedbf7ca0a50552f8a78dc98e996e366d
Arthur Pigeon <pigeon.arthur@hotmail.fr>
2022-07-01 11:38:11 +0200

    add vuetify

--- src/main.js
+++ src/main.js
@@ -3,18 +3,21 @@ import VueCompositionAPI from '@vue/composition-api'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+import vuetify from './plugins/vuetify'
 
 const API = null;
 
 new Vue({
   router,
   render: h => h(App),
+  vuetify,
+
   data() {
     return {
       username: '',
       isConnected: false
     }
-  },
+  }
 }).$mount("#app")
 
 Vue.use(VueCompositionAPI)
